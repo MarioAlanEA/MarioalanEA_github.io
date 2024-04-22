13f7061a122e698b14037329841a2e4241ce8ba3
MarioAlanEA <160806064+MarioAlanEA@users.noreply.github.com>
2024-04-22 14:27:48 -0700

    initial commit

--- rompecabezas.js
+++ rompecabezas.js
@@ -1,18 +1,19 @@
-var select = false; // Variable para controlar si se ha seleccionado una pieza
-var c = 'inc'; // Variable para controlar el estado de la acción de cambiar fondo
-var pos_s = ''; // Posición de la pieza seleccionada
-var id_s = ''; // ID de la pieza seleccionada
+var select = false; // variable para seleccionar una pieza 
+var c = 'inc'; // cambia el fondo
+var pos_s = ''; // posicion de pieza
+var id_s = ''; // pieza seleccionada 
+var imageUrl = 'albums/data.jpg'; // imagen principal 
 
 var rompecabezas = {
-    _arr_pos_r : new Array(), // Array para almacenar posiciones correctas
-    _arr_pos_a : new Array(), // Array para almacenar posiciones actuales
+    _arr_pos_r: new Array(), // arreglo posiciones correctas
+    _arr_pos_a: new Array(), // arreglo posiciones actuales
 
-    // Método para mostrar el rompecabezas en la página
-    _mostrar: function(){
+    //mostrar el rompecabezas 
+    _mostrar: function () {
         rompecabezas._arr_pos_r.length = 0;
         var piezas = rompecabezas._get('piezas').value;
         var tb = document.createElement('table');
-        // Configuración de la tabla
+        // tabla
         tb.border = 1;
         tb.align = 'center';
         tb.cellPadding = 0;
@@ -23,27 +24,29 @@ var rompecabezas = {
         dp.style.display = 'none'; // Oculta las coordenadas
         var ar = Math.sqrt(piezas);
         var c = 0;
-        var tam_img = 300;
+        var tam_img = 300; // Tamaño de la imagen 
         var pos_img = tam_img / ar;
-        for(var fil=1;fil<=ar;fil++){
+
+        for (var fil = 1; fil <= ar; fil++) {
             var tr = document.createElement('tr');
-            for(var cel=1;cel<=ar;cel++){
+            for (var cel = 1; cel <= ar; cel++) {
                 c++;
                 var td = document.createElement('td');
                 td.className = 'pieza';
-                td.id = 'pos_'+c;
-                td.style.width = pos_img+'px';
-                td.style.height = pos_img+'px';
+                td.id = 'pos_' + c;
+                td.style.width = pos_img + 'px';
+                td.style.height = pos_img + 'px';
                 var dbp = document.createElement('div');
-                dbp.id = 'val_bp_'+c;
-                var pX = -(pos_img * (cel - 1)) + 'px'; // Ajusta la posición X
-                var pY = -(pos_img * (fil - 1)) + 'px'; // Ajusta la posición Y
-                var p = pX + ' ' + pY; // Combinar las posiciones X e Y para formar la posición de fondo
+                dbp.id = 'val_bp_' + c;
+                var pX = -(pos_img * (cel - 1)) + 'px'; // x 
+                var pY = -(pos_img * (fil - 1)) + 'px'; // y
+                var p = pX + ' ' + pY; // x + y 
+                td.style.backgroundImage = 'url(' + imageUrl + ')';
                 td.style.backgroundPosition = p;
                 rompecabezas._arr_pos_r.push(p);
                 dbp.innerHTML = p;
                 dp.appendChild(dbp);
-                td.onclick = function(){
+                td.onclick = function () {
                     rompecabezas._cambiaBGP(this.id);
                     rompecabezas._compruebaFin();
                 }
@@ -51,13 +54,14 @@ var rompecabezas = {
             }
             tb.appendChild(tr);
         }
-        if(!rompecabezas._get('div_content')){
+
+        if (!rompecabezas._get('div_content')) {
             var cont = document.createElement('div');
             cont.id = 'div_content';
             cont.appendChild(tb);
             cont.appendChild(dp);
             document.body.appendChild(cont);
-        }else{
+        } else {
             rompecabezas._get('div_content').innerHTML = '';
             rompecabezas._get('div_content').appendChild(tb);
             rompecabezas._get('div_content').appendChild(dp);
@@ -67,8 +71,8 @@ var rompecabezas = {
         }
     },
 
-    // Método para mezclar las piezas
-    _mezclar: function(){
+    // funcion de mezclar muejejejejejjejejeje
+    _mezclar: function () {
         var num_alt = null;
         var arr = new Array();
         var resp = 'no';
@@ -76,106 +80,138 @@ var rompecabezas = {
         var repite = 'no';
         var pie = parseInt(rompecabezas._get('piezas').value);
         var pie1 = pie + 1;
-        while(arr.length < pie){
+        while (arr.length < pie) {
             repite = 'no';
-            num_alt = Math.floor(Math.random()*pie1);
-            if(num_alt != 0){
-                if(arr.length == 0){
+            num_alt = Math.floor(Math.random() * pie1);
+            if (num_alt != 0) {
+                if (arr.length == 0) {
                     i++;
                     arr[i] = num_alt;
-                }else{
-                    for(j=0;j<=arr.length-1;j++){
-                        if(arr[j] == num_alt){
+                } else {
+                    for (j = 0; j <= arr.length - 1; j++) {
+                        if (arr[j] == num_alt) {
                             repite = 'si';
                         }
                     }
-                    if(repite != 'si'){
+                    if (repite != 'si') {
                         i++;
                         arr[i] = num_alt;
                     }
                 }
             }
         }
-
-        var id = 0;    
-        for(k=0; k<=arr.length-1;k++){
-            id = k-(-1);
-            rompecabezas._get('pos_'+id).style.backgroundPosition = rompecabezas._get('val_bp_'+arr[k]).innerHTML;
+        // ciclo licuadora 
+        var id = 0;
+        for (k = 0; k <= arr.length - 1; k++) {
+            id = k - (-1);
+            rompecabezas._get('pos_' + id).style.backgroundPosition = rompecabezas._get('val_bp_' + arr[k]).innerHTML;
         }
 
-        // Limpiar el contenido del resultado
+        // limpia con luis miguel 
         var resultadoDiv = rompecabezas._get('resultado');
         resultadoDiv.innerHTML = '';
     },
 
     // Método para cambiar el fondo de una pieza al hacer clic
-    _cambiaBGP: function(id){
-        if(select == false){
+    _cambiaBGP: function (id) {
+        if (select == false) {
             pos_s = rompecabezas._get(id).style.backgroundPosition;
             id_s = id;
             select = true;
             rompecabezas._get(id_s).style.boxShadow = '1px 1px 14px #FFF,-1px -1px 14px #FFF, 1px -1px 14px #FFF,-1px 1px 14px #FFF';
-        }else{
-            var pos_n =  rompecabezas._get(id).style.backgroundPosition;
+        } else {
+            var pos_n = rompecabezas._get(id).style.backgroundPosition;
             var id_n = id;
             c = 'com';
             select = false;
         }
-     
-        if(c == 'com'){ rompecabezas._get(id_n).style.backgroundPosition = pos_s; rompecabezas._get(id_s).style.backgroundPosition = pos_n;
+
+        if (c == 'com') {
+            rompecabezas._get(id_n).style.backgroundPosition = pos_s;
+            rompecabezas._get(id_s).style.backgroundPosition = pos_n;
             c = 'inc';
             rompecabezas._get(id_s).style.boxShadow = '';
         }
     },
 
     // Método para comprobar si se ha completado el rompecabezas
-    _compruebaFin: function(){
-        var pie = parseInt(rompecabezas._get('piezas').value);
-        var fin = false;
-        rompecabezas._arr_pos_a.length = 0;
-        for(var i=1;i<=pie;i++){
-            rompecabezas._arr_pos_a.push(rompecabezas._get('pos_'+i).style.backgroundPosition);
+_compruebaFin: function () {
+    var pie = parseInt(rompecabezas._get('piezas').value);
+    var fin = false;
+    rompecabezas._arr_pos_a.length = 0;
+    for (var i = 1; i <= pie; i++) {
+        rompecabezas._arr_pos_a.push(rompecabezas._get('pos_' + i).style.backgroundPosition);
+    }
+    for (var j = 0; j < rompecabezas._arr_pos_r.length; j++) {
+        if (rompecabezas._arr_pos_r[j] != rompecabezas._arr_pos_a[j]) {
+            fin = false;
+            break;
+        } else {
+            fin = true;
         }
-        for(var j=0;j<rompecabezas._arr_pos_r.length;j++){
-            if(rompecabezas._arr_pos_r[j] != rompecabezas._arr_pos_a[j]){
-                fin = false;
-                break;
-            }else{
-                fin = true;
-            }
-        }
-        
-        if(fin){
-            var resultadoDiv = rompecabezas._get('resultado');
-            resultadoDiv.innerHTML = ''; // Limpiar el contenido previo
+    }
 
-            // Crear la imagen y el texto
-            var imagen = document.createElement('img');
-            imagen.style.width = '200px'; // Ajusta el tamaño de la imagen según sea necesario
-            var texto = document.createElement('p');
-            texto.textContent = 'data es un album del exitoso productor tainy, es un del genero urbano publicado en 2023 contando con 20 canciones contando con colabraciones de los artitas mas grandes en el genero urbano como bad bunny, rauw alejandro, jhayco, daddy yankee, feid etc. o con otros artistas fuera del genero urbano como julieta venegas y skrillex. El album es un album muy completo llevando a los limites los sonidos que puede tener el genero urbano teniendo sonidos como el reggeton, trap,pop chillwave,R&B alternativo,synthpop, el concepto del album va de sena una mujer robot que no tiene sentimiento, tainy le da sentimientos en forma de data, en lo personal es mi album favorito y una pieza que se merece una oportunidad de ser escuchado, siendo mis canciones preferidas FANTASMA/AVC, VOLVER, SI PREGUNTAS POR MI, SCI-FI, PARANORMAL'; // El texto que deseas mostrar
-            resultadoDiv.appendChild(imagen);
-            resultadoDiv.appendChild(texto);
-        }
-    },
+    if (fin) {
+        var resultadoDiv = rompecabezas._get('resultado');
+        resultadoDiv.innerHTML = ''; // escoba 
+
+        var texto = document.createElement('p');
+        texto.textContent = 'Felicidades, lo resolviste :)'; //cambialo 
+        texto.style.textAlign = 'center';
+        resultadoDiv.appendChild(texto);
+    }
+},
 
     // Método para obtener un elemento por su ID
-    _get: function(id){
+    _get: function (id) {
         return document.getElementById(id);
+    },
+
+    // funcion donde tu puedes subir tu imagen
+    _cargarImagen: function (event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function (readerEvent) {
+            var img = new Image(); //cambia la original por la nueva
+            img.src = readerEvent.target.result;
+
+            img.onload = function () {
+                var canvas = document.createElement('canvas');
+                var ctx = canvas.getContext('2d');
+
+                // estatura 
+                canvas.width = 300;
+                canvas.height = 300;
+
+                // Dibuja la imagen en el canvas con el tamaño ajustado
+                ctx.drawImage(img, 0, 0, 300, 300);
+
+                // cambia la ruta de la img
+                imageUrl = canvas.toDataURL('image/png');
+
+                //lo pasa al rompecabeza
+                rompecabezas._mostrar();
+                rompecabezas._mezclar();
+            };
+        };
+
+        reader.readAsDataURL(file);
     }
 };
 
-window.onload = function(){
-    rompecabezas._mostrar(); // Mostrar el rompecabezas al cargar la página
-    rompecabezas._mezclar(); // Mezclar las piezas al cargar la página
+window.onload = function () {
+    rompecabezas._mostrar(); // Mostrar
+    rompecabezas._mezclar(); // Mezclar
     // Configuración de eventos
-    rompecabezas._get('piezas').onchange = function(){
+    rompecabezas._get('piezas').onchange = function () {
         rompecabezas._mostrar();
     };
-    rompecabezas._get('mezclar').onclick = function(){
+    rompecabezas._get('mezclar').onclick = function () {
         rompecabezas._mezclar();
     };
+
+    //  carga de imagen
+    var imageUpload = document.getElementById('imageUpload');
+    imageUpload.addEventListener('change', rompecabezas._cargarImagen);
 };
-
-
-
